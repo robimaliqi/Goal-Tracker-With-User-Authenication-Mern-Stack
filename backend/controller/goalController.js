9eddd635149455b698bcebcb869fa2374837a4e3
Robi <robi.maliqi@hotmail.co.uk>
2022-08-01 08:40:09 +0100

    Create Controller
    * Create controller folder, create
      goalController.js file.
    * Create a function for each request inside
      the file and bring all the functions of each
      request from the goalRoutes.js file to the
      goalController.js file.
    * Use module.exports in the goalController file
      to export the data into goalRoutes and use
      require in the goalRoutes file.

---
+++ backend/controller/goalController.js
@@ -0,0 +1,34 @@
+// @desc Get goals (after we authentication it only add a specific user goal)
+// @route GET /api/goals
+// @acess Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+};
+
+// @desc Set goals
+// @route POST /api/goals
+// @acess Private
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Set goals" });
+};
+
+// @desc  Update goal
+// @route PUT /api/goals/:id
+// @acess Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+// @desc Delete goal
+// @route DELETE /api/goals/:id
+// @acess Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
