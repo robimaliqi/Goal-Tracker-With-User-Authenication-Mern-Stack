f49868d96013d2ed1401a8f2e5030bb20c2a3792
Robi <robi.maliqi@hotmail.co.uk>
2022-08-22 22:26:37 +0100

    Revert "React router set up"
    This reverts commit 2a335308e099fcc93b539fac2cf194a824a36e9c.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-
 function App() {
   return (
-    <>
-      <Router>
-        <div className="container">
-          <Routes>
-            <Route pasth="/" element={<Dashboard />} />
-            <Route pasth="/" element={<Login />} />
-            <Route pasth="/" element={<Register />} />
-          </Routes>
-        </div>
-      </Router>
-    </>
+    <div>
+      <h1>My App</h1>
+    </div>
   );
 }
 
