a833bc001d4a08d51e626e33e7a1e34f3c140ec2
Robi <robi.maliqi@hotmail.co.uk>
2022-09-12 18:26:30 +0100

    Goals slice in Redux
    * Add goals to global state.
    * In src, app, features we add a goals folder in
      that we add a goalService and goalSlice file.
    * In goalSlice we import createSlice and
      createAsyncThunk. Then we create a initialState
      which is an onject and includes goals, isError,
      isSuccess, isLoading and message. Then we
      create the slice and set it to goalSlice which
      takes in an object of name, initialState and
      reducers. Finally we export reset from
      sliceActions and the reducer.
    * In app, store.js we import the goalReducers and
      add it to the reducer object.

---
+++ frontend/src/features/goals/goalSlice.js
@@ -0,0 +1,20 @@
+import { createSlice, createAsynceThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  goals: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+export const goalSlice = createSlice({
+  name: "goal",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+});
+
+export const { reset } = goalSlice.actions;
+export default goalSlice.reducer;
