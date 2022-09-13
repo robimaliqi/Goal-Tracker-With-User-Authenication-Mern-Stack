4318e5052cb5a17db2c4382753d21cfd212f455f
Robi <robi.maliqi@hotmail.co.uk>
2022-09-13 18:22:36 +0100

    Create Goal Extra Reducer
    * In src, app, features, goalSlice create a thunk
      function. export createGoal and asigin it to
      createAsyncThunk and pass in the route/action
      name, secondly,  we want to pass in a async
      function which will take in the goalData and
      thunkAPI, then we throw a try catch in and copy
      the same catch error message from auth slice
      and paste it in here, also in the try we pass
      in our thunkAPI and set it to token and our
      goalService.
    * In goalService we import axios, create a API
      URL with a route assigned. Create a async which
      takes in goalData, token and an object with
      headers and Authorization which we asign to a
      bearer token and assign this function to
      createGoal, also create a variable for the
      response and equal to await axios with post
      request to the API URL and pass in our goalData
      and config (which has the headers), finally
      return the response.data and export the
      goalService.

---
+++ frontend/src/features/goals/goalService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+constAPI_URL = "/api/goals/";
+
+// Create new goal
+const createGoal = async (goalData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.post(API_URL, goalData, config);
+
+  return response.data;
+};
+
+const goalService = {
+  createGoal,
+};
+
+export default goalService;
--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsynceThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
   goals: [],
@@ -7,6 +7,25 @@ const initialState = {
   isLoading: false,
   message: "",
 };
+
+// Create new goal
+export const createGoal = createAsyncThunk(
+  "goals/create",
+  async (goalData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await goalService.createGoal(goalData, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const goalSlice = createSlice({
   name: "goal",
