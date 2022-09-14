7a6b0a0da1144cd2f6e22da49c881eff30a876b8
Robi <robi.maliqi@hotmail.co.uk>
2022-09-14 22:16:29 +0100

    Display Goals
    * Add a function in Dashboard, that if there is
      no goals it gives us a no goals message but if
      there is it shows it from the the new component
      we create called GoalItem.
    * In the goalItem component we create  we add a
      created at function to it and call the goal
      text.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -41,6 +41,17 @@ function Dashboard() {
         <p>Goals Dashboard</p>
       </section>
       <GoalForm />
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
   );
 }
